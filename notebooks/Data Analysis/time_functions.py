6a3c676851cb4812c28a108112598b892092ef14
brayden_dearden <braydendearden1@gmail.com>
2026-06-01 14:23:57 -0600

    Minor changes to formatting and variable names
    Some very small changes to formatting, variable names, and doc strings to improve readability.

--- notebooks/Data Analysis/time_functions.py
+++ notebooks/Data Analysis/time_functions.py
@@ -3,11 +3,11 @@ import cv2
 from imutils.video import FileVideoStream
 from datetime import datetime, timedelta
 
-reader = easyocr.Reader(['en'])
+reader = easyocr.Reader(["en"])
 
 
 def time_converter(video_file, x_fac, y_fac):
-    '''
+    """
     Gives the time of day for each of the frames in a video
 
         Args:
@@ -16,7 +16,7 @@ def time_converter(video_file, x_fac, y_fac):
             y_fac (int): specifies the scaling factor for the y-axis
         Returns:
             output (list): list of times in seconds, with index + 1 corresponding to the frame number
-    '''
+    """
     fvs = FileVideoStream(video_file).start()
     output = []
     frame_count = 0
@@ -28,31 +28,38 @@ def time_converter(video_file, x_fac, y_fac):
         frame = frame[20:76, 1443:1520]
         if frame_count == 1:
             # year, month, and day are arbitrary
-            t1 = datetime(2025, 8, 1, int(video_file[82:84]), int(
-                video_file[84:86]), int(video_file[86:88]))
-            output.append(t1)
+            time = datetime(
+                2025,
+                8,
+                1,
+                int(video_file[82:84]),
+                int(video_file[84:86]),
+                int(video_file[86:88]),
+            )
+            output.append(time)
             continue
         frame = cv2.resize(frame, None, fx=x_fac, fy=y_fac)
-        secs = str(t1.second).zfill(2) + \
-            str((t1 + timedelta(seconds=1)).second).zfill(2)
+        secs = str(time.second).zfill(2) + str(
+            (time + timedelta(seconds=1)).second
+        ).zfill(2)
         second = reader.readtext(frame, allowlist=secs, detail=0)
         if second:
             if second[0] == secs[2:]:
                 if frame_count > 15:
                     if output[-1] == output[-14]:
-                        t1 += timedelta(seconds=1)
+                        time += timedelta(seconds=1)
                 else:
-                    t1 += timedelta(seconds=1)
+                    time += timedelta(seconds=1)
         else:
             if frame_count >= 16:
                 if output[-1] == output[-15]:
-                    t1 += timedelta(seconds=1)
-        output.append(t1)
+                    time += timedelta(seconds=1)
+        output.append(time)
     return output
 
 
 def time_replace(direction_data_file, output):
-    '''
+    """
     Replaces frame number with corresponding time value in seconds
 
         Args:
@@ -61,23 +68,25 @@ def time_replace(direction_data_file, output):
                 keys: fish tag number
                 values (list): list of frame number for crossing times
             output (list): list of time stamps in seconds for the file
-                As returned by time_converter() function
+                As returned by time_converter() function where the index equals the frame count - 1
         Returns:
             new_data (dict): same structure as direction_data_file but with time values in seconds
-    '''
+    """
     new_data = {}
     for tag, times in direction_data_file.items():
         new_data[tag] = []
         for time in times:
-            if output[time-1] not in new_data[tag]:
-                stamp = output[time-1]
-                new_data[tag].append(3600 * stamp.hour +
-                                     60 * stamp.minute + stamp.second)
+            if output[time - 1] not in new_data[tag]:
+                stamp = output[time - 1]
+                new_data[tag].append(
+                    3600 * stamp.hour + 60 * stamp.minute + stamp.second
+                )
     return new_data
 
 
 def direction_time_replace(lr, rl, video_files, x_fac, y_fac):
-    '''Uses time_replace function for all files in the crossing data
+    """
+    Uses time_replace function for all files in the crossing data
 
         Args:
             lr (dict): gives files where fish crossed the line left to right
@@ -98,25 +107,25 @@ def direction_time_replace(lr, rl, video_files, x_fac, y_fac):
         Returns:
             lr (dict): same structure as parameter but with times in seconds
             rl (dict): same structure as parameter but with times in seconds
-    '''
-    files = []
+    """
+    file_list = []
     for file in lr:
-        files.append(file)
+        file_list.append(file)
         output = time_converter(video_files[file], x_fac, y_fac)
         lr[file] = time_replace(lr[file], output)
         if file in rl:
             rl[file] = time_replace(rl[file], output)
     for file in rl:
-        if file in files:
+        if file in file_list:
             continue
-        files.append(file)
+        file_list.append(file)
         output = time_converter(video_files[file], x_fac, y_fac)
         rl[file] = time_replace(rl[file], output)
     return lr, rl
 
 
 def full_day(lr, rl):
-    '''
+    """
     Combines direction data from all of the files for a day
 
         Args:
@@ -137,13 +146,13 @@ def full_day(lr, rl):
             full_rl (dict): fish with their respective crossing times in seconds
                 keys: fish tag number
                 values: list of crossing times in seconds
-    '''
+    """
     full_lr = {}
     full_rl = {}
-    for file in lr:
-        for tag, times in lr[file].items():
+    for file_data in lr.values():
+        for tag, times in file_data.items():
             full_lr[tag] = times
-    for file in rl:
-        for tag, times in rl[file].items():
+    for file_data in rl.values():
+        for tag, times in file_data.items():
             full_rl[tag] = times
     return full_lr, full_rl
