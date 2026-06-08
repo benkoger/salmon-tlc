e516202692c25a01be6d081ec17b49e7ba1a8093
brayden_dearden <braydendearden1@gmail.com>
2026-06-08 13:54:51 -0600

    Update time_converter function
    Update to time _converter function so that the formatting works on the actual data. In the future, more edits might be made to the frame processing to increase accuracy of OCR reader. Additionally, minor changes were made in the full_day function to improve readability and conciseness.

--- notebooks/Data Analysis/time_functions.py
+++ notebooks/Data Analysis/time_functions.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 reader = easyocr.Reader(["en"])
 
 
-def time_converter(video_file, x_fac, y_fac):
+def time_converter(video_file, x_fac, y_fac, folder_len):
     """
     Gives the time of day for each of the frames in a video
 
@@ -14,46 +14,60 @@ def time_converter(video_file, x_fac, y_fac):
             video_file: mp4 file of the salmon
             x_fac (int): specifies the scaling factor for the x-axis
             y_fac (int): specifies the scaling factor for the y-axis
+            folder_len (int): length of direction for folder to accurately extract initial time from file name
         Returns:
             output (list): list of times in seconds, with index + 1 corresponding to the frame number
     """
-    fvs = FileVideoStream(video_file).start()
+    crop_dict = {
+        "20250804": [1426, 1510],
+        "20250806": [1435, 1519],
+        "20250808": [1460, 1542],
+        "20250811": [1426, 1510],
+    }
     output = []
     frame_count = 0
+    date_info = video_file[folder_len + 11 : folder_len + 26]
+    if date_info[:8] in crop_dict:
+        x_s = crop_dict[date_info[:8]]
+    else:
+        x_s = [1442, 1524]
+    one_sec = timedelta(seconds=1)
+    fvs = FileVideoStream(video_file).start()
     while fvs.more():
         frame = fvs.read()
         if frame is None:
             break
         frame_count += 1
-        frame = frame[20:76, 1443:1520]
         if frame_count == 1:
             # year, month, and day are arbitrary
             time = datetime(
-                2025,
-                8,
-                1,
-                int(video_file[82:84]),
-                int(video_file[84:86]),
-                int(video_file[86:88]),
+                int(date_info[:4]),
+                int(date_info[4:6]),
+                int(date_info[6:8]),
+                int(date_info[9:11]),
+                int(date_info[11:13]),
+                int(date_info[13:]),
             )
             output.append(time)
             continue
+        frame = frame[20:76, x_s[0] : x_s[1]]
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame = cv2.resize(frame, None, fx=x_fac, fy=y_fac)
-        secs = str(time.second).zfill(2) + str(
-            (time + timedelta(seconds=1)).second
-        ).zfill(2)
-        second = reader.readtext(frame, allowlist=secs, detail=0)
+        secs = str(time.second).zfill(2) + str((time + one_sec).second).zfill(2)
+        second = reader.readtext(
+            frame, allowlist=secs, detail=0
+        )  # , text_threshold=0.6, low_text=0.3)
         if second:
             if second[0] == secs[2:]:
                 if frame_count > 15:
                     if output[-1] == output[-14]:
-                        time += timedelta(seconds=1)
+                        time += one_sec
                 else:
-                    time += timedelta(seconds=1)
+                    time += one_sec
         else:
             if frame_count >= 16:
                 if output[-1] == output[-15]:
-                    time += timedelta(seconds=1)
+                    time += one_sec
         output.append(time)
     return output
 
@@ -150,9 +164,7 @@ def full_day(lr, rl):
     full_lr = {}
     full_rl = {}
     for file_data in lr.values():
-        for tag, times in file_data.items():
-            full_lr[tag] = times
+        full_lr.update(file_data)
     for file_data in rl.values():
-        for tag, times in file_data.items():
-            full_rl[tag] = times
+        full_rl.update(file_data)
     return full_lr, full_rl
