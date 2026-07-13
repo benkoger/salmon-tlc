2772f9dd3e13321b6cd4a42b18895696c4815ce5
brayden_dearden <braydendearden1@gmail.com>
2026-07-13 15:21:45 -0600

    Changes to allow addition of Big camera
    Changes in functions to ensure that "upstream" does not have to be going from right to left, thereby allowing the data from Big to be analyzed in a similar way to the other cameras thus far.

--- notebooks/Data Analysis/time_plot_functions.py
+++ notebooks/Data Analysis/time_plot_functions.py
@@ -12,11 +12,12 @@ import numpy as np
 
 from cross_group_functions import tracks_crossing_info, dbscan_time_groups
 
-cam_names = [
-    "cam01-bear_outflow",
-    "cam07-grass_outflow",
-    "cam09-trail_outflow",
-]
+cam_names = {
+    "cam01-bear_outflow": False,
+    "cam07-grass_outflow": False,
+    "cam0610-big_outflow": True,
+    "cam09-trail_outflow": False,
+}
 
 
 def closest_all_fish_distances(
@@ -39,15 +40,17 @@ def closest_all_fish_distances(
     xscale = "log" if xlog else "linear"
     yscale = "log" if ylog else "linear"
     fig = plt.figure(figsize=(15, 15))
-    for i, cam in enumerate(cam_names):
+    for i, (cam, leftright) in enumerate(cam_names.items()):
         folder = f"/project/uwyo-0003/salmon-tlc/processing/tracks-with-true-times_06-18-2026/{cam}"
         tracks_files = sorted(glob.glob(os.path.join(folder, "*.pkl")))
         close_dists = []
         all_dists = []
         for tracks_file in tracks_files:
-            rl = tracks_crossing_info(tracks_file, 1250)
+            crossing_data = tracks_crossing_info(tracks_file, 1250, leftright)
             dists_dict = defaultdict(list)
-            for (tag1, times1), (tag2, times2) in combinations(rl.items(), 2):
+            for (tag1, times1), (tag2, times2) in combinations(
+                crossing_data.items(), 2
+            ):
                 min_dist = 100000  # arbitrarily large
                 for time1 in times1:
                     for time2 in times2:
@@ -59,7 +62,7 @@ def closest_all_fish_distances(
                 close_dists.append(min(dists))
                 for dist in dists:
                     all_dists.append(dist)
-        ax = fig.add_subplot(3, 2, 2 * i + 1, xscale=xscale, yscale=yscale)
+        ax = fig.add_subplot(len(cam_names), 2, 2 * i + 1, xscale=xscale, yscale=yscale)
         close_dists = np.array(close_dists)
         if xlog:
             _, bins = np.histogram(close_dists[close_dists > 0], bins=num_bins)
@@ -72,7 +75,7 @@ def closest_all_fish_distances(
         ax.set_ylabel("Number of fish")
         if not all_distances:
             continue
-        ax = fig.add_subplot(3, 2, 2 * i + 2, xscale=xscale, yscale=yscale)
+        ax = fig.add_subplot(len(cam_names), 2, 2 * i + 2, xscale=xscale, yscale=yscale)
         all_dists = np.array(all_dists)
         if xlog:
             _, bins = np.histogram(all_dists[all_dists > 0], bins=num_bins)
@@ -104,21 +107,21 @@ def interfish_time_dist_plot(xlog, ylog, altogether, save, line=1250):
     """
     xscale = "log" if xlog else "linear"
     yscale = "log" if ylog else "linear"
-    fig = plt.figure(figsize=(15, 8))
+    fig = plt.figure(figsize=(8, 8))
     full_distances = []
-    for i, cam in enumerate(cam_names):
+    for i, (cam, leftright) in enumerate(cam_names.items()):
         folder = f"/project/uwyo-0003/salmon-tlc/processing/tracks-with-true-times_06-18-2026/{cam}"
         tracks_files = sorted(glob.glob(os.path.join(folder, "*.pkl")))
         interfish_distances = []
         for tracks_file in tracks_files:
-            rl = tracks_crossing_info(tracks_file, line)
-            all_tags = list(rl)
+            crossing_data = tracks_crossing_info(tracks_file, line, leftright)
+            all_tags = list(crossing_data)
             for j, tag in enumerate(all_tags):
                 if j == len(all_tags) - 1:
                     continue
                 distances = []
-                for time1 in rl[tag]:
-                    for time2 in rl[all_tags[j + 1]]:
+                for time1 in crossing_data[tag]:
+                    for time2 in crossing_data[all_tags[j + 1]]:
                         distances.append(abs(time1 - time2))
                 interfish_distances.append(min(distances))
         if not interfish_distances:
@@ -127,7 +130,7 @@ def interfish_time_dist_plot(xlog, ylog, altogether, save, line=1250):
             full_distances += interfish_distances
             continue
         interfish_distances = np.array(interfish_distances)
-        ax = fig.add_subplot(3, 1, i + 1, xscale=xscale, yscale=yscale)
+        ax = fig.add_subplot(len(cam_names), 1, i + 1, xscale=xscale, yscale=yscale)
         if xlog:
             _, bins = np.histogram(interfish_distances[interfish_distances > 0])
             bins = np.logspace(np.log10(bins[0]), np.log10(bins[-1]), len(bins))
@@ -176,16 +179,16 @@ def fish_group_distr_hist(
             plots a histogram from the empirical data, based on the given arguments
     """
     all_sizes = []
-    for cam in cam_names:
+    for cam, leftright in cam_names.items():
         folder = f"/project/uwyo-0003/salmon-tlc/processing/tracks-with-true-times_06-18-2026/{cam}"
         tracks_files = sorted(glob.glob(os.path.join(folder, "*.pkl")))
         cam_sizes = []
         for tracks_file in tracks_files:
-            rl = tracks_crossing_info(tracks_file, line)
-            groups = dbscan_time_groups(rl, proximity)
+            crossing_data = tracks_crossing_info(tracks_file, line, leftright)
+            groups = dbscan_time_groups(crossing_data, proximity)
             if size1:
                 group_fish = [fish for group in groups for fish in group]
-                for tag in rl:
+                for tag in crossing_data:
                     if tag not in group_fish:
                         groups.append([tag])
             if weighted_groups:
@@ -208,20 +211,14 @@ def fish_group_distr_hist(
             cam_sizes, bins=bins, weights=np.ones(len(cam_sizes)) / len(cam_sizes)
         )
         all_counts.append(counts)
-    colors = ["tab:blue", "tab:orange", "#009E73"]
-    for i, counts in enumerate(all_counts):
-        plt.bar(
-            x + (i - 1) * width,
-            counts,
-            width=width,
-            label=f"{cam_names[i]}",
-            color=colors[i],
-        )
+    colors = ["tab:blue", "tab:orange", "#009E73", "#CCB000"]
+    for i, (counts, cam) in enumerate(zip(all_counts, cam_names)):
+        plt.bar(x + (i - 1) * width, counts, width=width, label=cam, color=colors[i])
     start = "Probability distribution" if probability else "Histogram"
     then = "for individual fish" if weighted_groups else "for group sizes"
     plt.title(f"{start} {then} across cameras")
     plt.legend()
-    plt.xticks(x)
+    # plt.xticks(x)
     if save:
         first = "probability" if probability else "hist"
         second = "individuals" if weighted_groups else "group-sizes"
@@ -232,18 +229,18 @@ def fish_group_distr_hist(
 def make_lollipop_plots(proximity, save, total_plot=False, line=1250):
     dates = ["2025-08-07", "2025-08-10"]
     fig = plt.figure(figsize=(25, 15))
-    num_rows = 6 if total_plot else 3
+    num_rows = len(cam_names) * 2 if total_plot else len(cam_names)
     cam_repeat = 4 if total_plot else 2
-    for i, cam in enumerate(cam_names):
+    for i, (cam, leftright) in enumerate(cam_names.items()):
         folder = f"/project/uwyo-0003/salmon-tlc/processing/tracks-with-true-times_06-18-2026/{cam}"
         tracks_files = sorted(glob.glob(os.path.join(folder, "*.pkl")))
         for j, tracks_file in enumerate([tracks_files[3], tracks_files[6]]):
-            rl = tracks_crossing_info(tracks_file, line)
+            crossing_data = tracks_crossing_info(tracks_file, line, leftright)
             total = 0
             total_fish = []
             total_times = []
             if total_plot:
-                for tag, times in rl.items():
+                for tag, times in crossing_data.items():
                     total += 1
                     total_fish.append(total)
                     total_times.append(np.mean(times) / 3600)
@@ -254,18 +251,20 @@ def make_lollipop_plots(proximity, save, total_plot=False, line=1250):
                 if i == 0:
                     ax.set_ylabel("Number of fish crossed")
                 ax.set_title(f"Total fish crossing over {dates[j]} for {cam}")
-            groups = dbscan_time_groups(rl, proximity)
+            groups = dbscan_time_groups(crossing_data, proximity)
             group_fish = [fish for group in groups for fish in group]
             singles = []
-            for tag in rl:
+            for tag in crossing_data:
                 if tag not in group_fish:
                     singles.append(tag)
             group_times = []
             for group in groups:
-                group_times.append([np.mean(rl[group[0]]) / 3600, len(group)])
+                group_times.append(
+                    [np.mean(crossing_data[group[0]]) / 3600, len(group)]
+                )
             single_times = []
             for single in singles:
-                single_times.append([np.mean(rl[single]) / 3600, 1])
+                single_times.append([np.mean(crossing_data[single]) / 3600, 1])
             group_times = np.array(group_times)
             single_times = np.array(single_times)
             ax = fig.add_subplot(num_rows, 2, 1 + j + cam_repeat * i)
@@ -284,7 +283,9 @@ def make_lollipop_plots(proximity, save, total_plot=False, line=1250):
                     markerfmt="Dr",
                 )
             ax.set_xlim(15, 24)
-            ax.set_ylim(0, 8)
+            ax.set_ylim(
+                0,
+            )
             ax.set_title(f"Group sizes over time on {dates[j]} for {cam}")
             ax.set_xlabel("Time of day (hours)")
             if i == 0:
@@ -292,7 +293,7 @@ def make_lollipop_plots(proximity, save, total_plot=False, line=1250):
     plt.tight_layout()
     if save:
         last = "_with totals" if total_plot else ""
-        plt.save_fig(f"group_size_lollipop{last}-{proximity}.jpg", dpi=500)
+        plt.savefig(f"group_size_lollipop{last}-{proximity}.jpg", dpi=500)
     plt.show()
 
 
@@ -311,12 +312,14 @@ def gaussian_distr_check(save, line=1250, new_mid=18.5):
     stds = []
     x_vals = np.linspace(15, 24, 901)
     y = []
-    for cam in cam_names:
+    for cam, leftright in cam_names.items():
         folder = f"/project/uwyo-0003/salmon-tlc/processing/tracks-with-true-times_06-18-2026/{cam}"
         tracks_files = sorted(glob.glob(os.path.join(folder, "*.pkl")))
         for tracks_file in [tracks_files[3], tracks_files[6]]:
-            rl = tracks_crossing_info(tracks_file, line)
-            all_times = np.array([np.mean(times) / 3600 for times in rl.values()])
+            crossing_data = tracks_crossing_info(tracks_file, line, leftright)
+            all_times = np.array(
+                [np.mean(times) / 3600 for times in crossing_data.values()]
+            )
             all_times = all_times[all_times >= 15]
             all_times = [
                 time - (all_times[round(len(all_times) / 2)] - new_mid)
@@ -336,12 +339,17 @@ def gaussian_distr_check(save, line=1250, new_mid=18.5):
             means.append(me)
             stds.append(st)
             plt.plot(all_times, total_fish, color="gray", alpha=0.3)
+            # if cam == "cam0610-big_outflow":
+            #     plt.plot(all_times, total_fish, color="#009E73")
+            # else:
+            #     plt.plot(all_times, total_fish, color="gray", alpha=0.3)
             f = interp1d(all_times, total_fish, bounds_error=False, fill_value=(0, 1))
             y.append(f(x_vals))
     y_vals = np.nanmean(y, axis=0)
-    plt.plot(x_vals, y_vals, color="tab:blue", label="average")
+    plt.plot(x_vals, y_vals, color="tab:blue", label="mean")
 
-    avg, std_dev = new_mid, np.mean(stds)
+    avg, std_dev = np.mean(means), np.mean(stds)
+    print(avg)
     a, b = (15 - avg) / std_dev, (24 - avg) / std_dev
 
     plt.plot(
@@ -355,4 +363,49 @@ def gaussian_distr_check(save, line=1250, new_mid=18.5):
     plt.ylabel("Cumulative proportion of fish arriving")
     if save:
         plt.savefig("cummulative_prop-gaussian.jpg", dpi=500)
+    plt.show()
+
+
+def empirical_totals_plot(time_step, save, line=1250, new_mid=18.5):
+    """
+    Plots a figure with a subplot for each of the two days for each camera, showing the fraction of fish arriving at each time step
+
+        Args:
+            time_step (float): number of seconds per time step
+            save (bool): if True, the figure will be saved in the working directory
+            line (int): x-value of the vertical line used for crossing
+            new_mid (float): time location (in hours) for the new middle of the data once adjusted
+        Returns:
+            Plots a figure with subplots showing the percentage of fish arriving per time step for 08/07/2026 and 08/10/2026
+    """
+    t_vals = np.linspace(15, 24, int(9 * 3600 / time_step) + 1)
+    bins = np.linspace(15, 24, len(t_vals) - 1)
+    dates = ["2025-08-07", "2025-08-10"]
+    fig = plt.figure(figsize=(15, 10))
+    for i, (cam, leftright) in enumerate(cam_names.items()):
+        folder = f"/project/uwyo-0003/salmon-tlc/processing/tracks-with-true-times_06-18-2026/{cam}"
+        tracks_files = sorted(glob.glob(os.path.join(folder, "*.pkl")))
+        for j, tracks_file in enumerate([tracks_files[3], tracks_files[6]]):
+            crossing_data = tracks_crossing_info(tracks_file, line, leftright)
+            times = [np.mean(times) / 3600 for times in crossing_data.values()]
+            times = np.array(
+                [time - (times[round(len(times) / 2)] - new_mid) for time in times]
+            )
+            times = times[times >= 15]
+
+            hist_data = []
+            for time in times:
+                for n, t in enumerate(t_vals[1:]):
+                    if t_vals[n] < time <= t:
+                        hist_data.append(t)
+            ax = fig.add_subplot(len(cam_names), 2, 2 * i + j + 1)
+            ax.hist(
+                hist_data, bins=bins, weights=np.ones(len(hist_data)) / len(hist_data)
+            )
+            ax.set_title(f"{cam}: {dates[j]}")
+            ax.set_xlabel("Time (hours)")
+            ax.set_ylabel("Fraction of fish arriving")
+    plt.tight_layout()
+    if save:
+        plt.savefig(f"empirical_totals-days_and_cams-{time_step}.jpg", dpi=500)
     plt.show()
