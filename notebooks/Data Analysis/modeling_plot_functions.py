b37ed6a1e63887eb7c5f8f1e34be5e9cf444c0ad
brayden_dearden <braydendearden1@gmail.com>
2026-07-13 15:19:34 -0600

    New function for full figure, minor fixes for Big
     New function added that implements the new way of plotting the fourth and fifth plots as well as minor changes to ensure seamless addition of the data from Big.

--- notebooks/Data Analysis/modeling_plot_functions.py
+++ notebooks/Data Analysis/modeling_plot_functions.py
@@ -12,6 +12,159 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def make_full_figure_new(
+    step_size, alpha, gamma, save, line=1250, new_mid=18.5, iterations=1000
+):
+    t_vals = np.linspace(15, 24, int(9 * 3600 / step_size) + 1)
+    fig = plt.figure(figsize=(15, 10))
+    empirical_data = empirical_data_return(t_vals, line, new_mid)
+    asocial_data = asocial_data_return_true_size(t_vals)
+    social_data = social_data_return_true_size(t_vals, alpha, gamma)
+
+    names = ["empirical data", "asocial model", "social model"]
+    colors = ["tab:blue", "#009E73", "tab:orange"]
+
+    # for subplots a - c
+    bins = np.linspace(15, 24, len(t_vals) - 1)
+    hist_data = [empirical_data[1], asocial_data[1], social_data[1]]
+    for i, data in enumerate(hist_data):
+        ax = fig.add_subplot(2, 3, i + 1)
+        ax.hist(
+            data, bins=bins, weights=np.ones(len(data)) / len(data), color=colors[i]
+        )
+        ax.set_xlim(15, 24)
+        ax.set_ylim(0, 0.05)
+        ax.set_xlabel("Time (hours)")
+        ax.set_ylabel("Fraction of fish arriving")
+        ax.set_title(names[i])
+
+    range_index = round(0.05 * iterations / 2)
+    ax = fig.add_subplot(2, 3, 4)
+
+    emp_fracs, _ = np.histogram(
+        empirical_data[1],
+        bins=bins,
+        weights=np.ones(len(empirical_data[1])) / len(empirical_data[1]),
+    )
+    emp_y_vals = plot5_y_vals_return(empirical_data[0], t_vals)
+
+    asocial_fracs = []
+    asocial_y_vals = []
+    asocial_totals = []
+    social_fracs = []
+    social_y_vals = []
+    social_totals = []
+    for i in range(iterations):
+        asocial_data = asocial_data_return_true_size(t_vals)
+        asoc_fracs, _ = np.histogram(
+            asocial_data[1],
+            bins=bins,
+            weights=np.ones(len(asocial_data[1])) / len(asocial_data[1]),
+        )
+        asocial_fracs.append(asoc_fracs)
+        asocial_y_vals.append(plot5_y_vals_return(asocial_data[0], t_vals))
+
+        social_data = social_data_return_true_size(t_vals, alpha, gamma)
+        soc_fracs, _ = np.histogram(
+            social_data[1],
+            bins=bins,
+            weights=np.ones(len(social_data[1])) / len(social_data[1]),
+        )
+        social_fracs.append(soc_fracs)
+        social_y_vals.append(plot5_y_vals_return(social_data[0], t_vals))
+        if i < 20:
+            for totals in asocial_data[0]:
+                asocial_totals += [totals]
+            for totals in social_data[0]:
+                social_totals += [totals]
+    asoc_max = max([max(fracs) for fracs in asocial_fracs])
+    soc_max = max([max(fracs) for fracs in social_fracs])
+    max_frac = max([max(emp_fracs), asoc_max, soc_max])
+    bins = np.linspace(0, max_frac, 15)
+
+    for i, model_fracs in enumerate([asocial_fracs, social_fracs]):
+        x_vals, y_vals = [], []
+        for fracs in model_fracs:
+            heights, edges = np.histogram(fracs, bins=bins, density=True)
+            if not list(x_vals):
+                x_vals = (edges[:-1] + edges[1:]) / 2
+            y_vals.append(heights)
+        med_vals = np.median(y_vals, axis=0)
+        new_vals = [[] for _ in range(len(y_vals[0]))]
+        for vals in y_vals:
+            for j, y in enumerate(vals):
+                new_vals[j].append(y)
+        new_vals = [sorted(vals) for vals in new_vals]
+        lower = [vals[range_index] for vals in new_vals]
+        upper = [vals[-range_index] for vals in new_vals]
+        ax.semilogy(x_vals, med_vals, color=colors[i + 1], label=names[i + 1])
+        ax.fill_between(x_vals, lower, upper, color=colors[i + 1], alpha=0.15)
+    heights, edges = np.histogram(emp_fracs, bins=bins, density=True)
+    centers = (edges[:-1] + edges[1:]) / 2
+    ax.semilogy(centers, heights, color=colors[0], label=names[0])
+    ax.legend()
+    ax.set_xlabel("Fraction of daily run arriving per time step")
+    ax.set_ylabel("PDF")
+
+    ax = fig.add_subplot(2, 3, 5)
+    for i, y_vals in enumerate([asocial_y_vals, social_y_vals]):
+        med_vals = np.median(y_vals, axis=0)
+        new_vals = [[] for _ in range(len(y_vals[0]))]
+        for vals in y_vals:
+            for j, y in enumerate(vals):
+                new_vals[j].append(y)
+        new_vals = [sorted(vals) for vals in new_vals]
+        lower = [vals[range_index] for vals in new_vals]
+        upper = [vals[-range_index] for vals in new_vals]
+        ax.plot(t_vals, med_vals, color=colors[i + 1], label=names[i + 1])
+        ax.fill_between(t_vals, lower, upper, color=colors[i + 1], alpha=0.15)
+    ax.plot(t_vals, emp_y_vals, color=colors[0], label=names[0])
+    ax.legend()
+    ax.set_xlabel("Time (hours)")
+    ax.set_ylabel("SD (cummulative fraction arrived)")
+
+    names = ["empirical data", "social model", "asocial model"]
+    colors = ["tab:blue", "tab:orange", "#009E73"]
+    ax = fig.add_subplot(2, 3, 6)
+    raw_totals = [asocial_totals, social_totals, empirical_data[0]]
+    for i, model_totals in enumerate(raw_totals):
+        points = []
+        for totals in model_totals:
+            new_totals = [0]
+            for j, total in enumerate(totals[1:]):
+                new_totals.append(total - totals[j])
+            ruggedness = ruggedness_func(new_totals)
+            points.append([totals[-1], ruggedness])
+        if i == 2:
+            new_points = np.array(sorted(points))
+            X = sm.add_constant(new_points[:, 0])
+            model = sm.OLS(new_points[:, 1], X).fit()
+            pred = model.get_prediction(X)
+            ci = pred.summary_frame(alpha=0.05)
+            ax.plot(new_points[:, 0], ci["mean"], color=colors[2 - i])
+            ax.fill_between(
+                new_points[:, 0],
+                ci["mean_ci_lower"],
+                ci["mean_ci_upper"],
+                color=colors[2 - i],
+                alpha=0.15,
+            )
+        points = np.array(points)
+        ax.scatter(points[:, 0], points[:, 1], color=colors[2 - i], label=names[2 - i])
+    ax.legend()
+    ax.set_ylim(0, 1)
+    ax.set_xlabel("Daily run size")
+    ax.set_ylabel("Ruggedness")
+    fig.suptitle(
+        rf"{int( 9 * 3600 / (len(t_vals) - 1))}-second time steps with $\alpha$={alpha} and $\gamma$={gamma}"
+    )
+    if save:
+        plt.savefig(
+            f"{step_size}_seconds-{alpha}-{gamma}_empirical_run_size.jpg", dpi=500
+        )
+    plt.show()
+
+
 def make_full_figure(
     t_vals, empirical_data, asocial_data, social_data, alpha, gamma, save
 ):
